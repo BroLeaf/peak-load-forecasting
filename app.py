2388f163a1ae58aea42579c39682951055049a60
BroLeaf <dj3vu6zp@gmail.com>
2019-03-30 14:39:17 +0800

    Use support data to forecaste

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn import metrics
-
 # read data set
 data = pd.read_csv("2017.1.1 ~ 2019.1.31 的電力供需.csv")
 data = data[["日期","淨尖峰供電能力(MW)","尖峰負載(MW)","備轉容量(MW)","備轉容量率(%)"]]
@@ -17,13 +15,27 @@ y = data[["尖峰負載(MW)"]]
 
 
 # 75% 劃分為 train set, 25%劃分為 test set
-X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=1)
+# X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=1)
 
 # train module
 linreg = LinearRegression()
-linreg.fit(X_train,y_train)
+linreg.fit(X,y)
 
-y_pred = linreg.predict(X_test)
+# read support data
+pre_data = pd.read_csv("power_supply_predicted.csv")
+pre_data = pre_data.rename(index=str,columns={"日期(年/月/日)":"日期","預估淨尖峰供電能力(萬瓩)":"淨尖峰供電能力(MW)","預估瞬時尖峰負載(萬瓩)":"尖峰負載(MW)","預估尖峰備轉容量率(%)":"備轉容量率(%)","預估尖峰備轉容量(萬瓩)":"備轉容量(MW)"})
+# modify format
+for i in range(0, 7):
+    s=pre_data.at[str(i), "日期"]
+    pre_data.at[str(i), "日期"]=s[0:4]+s[5:7]+s[8:10]
+    pre_data.at[str(i), "淨尖峰供電能力(MW)"]*=10
+    pre_data.at[str(i), "備轉容量(MW)"]*=10
+pre_data = pre_data[["日期","淨尖峰供電能力(MW)","備轉容量(MW)","備轉容量率(%)"]]    
+# print(pre_data)
 
-print ("MSE:",metrics.mean_squared_error(y_test, y_pred))
-print ("RMSE:",np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+# predict future peak load for a week and output
+pred = linreg.predict(pre_data)
+pred_output = pd.DataFrame(pred, columns=["peak_load(MW)"])
+pred_output.insert(0,"date", pre_data["日期"].to_numpy())
+print(pred_output)
+pred_output.to_csv("submission.csv", encoding='utf-8', index=False)
